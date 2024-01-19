52a2cfc87939a69009174444ac6463caa6367c94
abdennuur <abdnourkhatib@gmail.com>
2024-01-19 07:58:30 -0800

    0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,46 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort arr lements frm the min to max value
- * @array: arr
- * @size: arr size
+ * swp_ints - Swp 2 ints in array.
+ * @a: first int to swap.
+ * @b: second int to swap.
+ */
+void swp_ints(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Sort an arr of ints frm min to max.
+ * @array: array of ints to sort.
+ * @size: size of array.
+ *
+ * Description: Print array after each swap.
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t ix, ln = size;
+	bool bubbly = false;
 
-	size_t ix, idx, temp = 0;
-
-	if (size < 2)
+	if (array == NULL || size < 2)
 		return;
-	for (ix = 0; ix < size; ix++)
-		for (idx = 0; idx < size; idx++)
+
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (ix = 0; ix < ln - 1; ix++)
 		{
-			if (array[idx] > array[idx + 1] && array[idx + 1])
+			if (array[ix] > array[ix + 1])
 			{
-			temp = array[idx];
-			array[idx] = array[idx + 1];
-			array[idx + 1] = temp;
-			print_array(array, size);
+				swap_ints(array + ix, array + ix + 1);
+				print_array(array, size);
+				bubbly = false;
 			}
 		}
+		ln--;
+	}
 }
